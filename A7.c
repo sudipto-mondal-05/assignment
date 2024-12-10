7d36e23c02ae5b0d5d23966adfb198435b3c8a7e
sudipto-mondal-05 <sudiptohwh2005@gmail.com>
2024-12-10 21:33:49 +0530

    A6 modified

--- A7.c
+++ A7.c
@@ -1,21 +1,17 @@
 // 1.
 
-// #include <stdio.h>
-
-// int main() {
-//     char str[100];
-//     int length = 0;
-
-//     printf("Enter a string: ");
-//     scanf("%[^\n]s", str);
-
-//     for (int i = 0; str[i] != '\0'; i++) {
-//         length++;
-//     }
-
-//     printf("Length of the string: %d\n", length);
-//     return 0;
-// }
+#include <stdio.h>
+int main() {
+    char str[200];
+    int length = 0;
+    printf("Enter a string: ");
+    gets(str); 
+    while (str[length] != '\0') {
+        length++; 
+    }
+    printf("Length of the string: %d\n", length);
+    return 0;
+}
 
 // 2.
 
