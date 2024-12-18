6d73bf4765560d0367730f3c2008312028050488
sudipto-mondal-05 <sudiptohwh2005@gmail.com>
2024-12-18 17:19:28 +0530

    final A6

--- A6.c
+++ A6.c
@@ -69,7 +69,7 @@ int main() {
 //     int n;
 //     printf("Enter the size of the square matrix: ");
 //     scanf("%d", &n);
-//     int matrix[n][n], symmetric = 1;
+//     int matrix[n][n], symmetric = 1; // in compilers 0 == false and 1 == true
 //     printf("Enter the elements of the matrix:\n");
 //     for (int i = 0; i < n; i++) {
 //         for (int j = 0; j < n; j++) {
