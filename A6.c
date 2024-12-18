67d1ccac3165c16a9d7fc5faf69e4412f1d6be34
sudipto-mondal-05 <sudiptohwh2005@gmail.com>
2024-12-18 17:20:37 +0530

    final A6

--- A6.c
+++ A6.c
@@ -27,39 +27,39 @@
 
 //Assignment 6(2)
 
-#include <stdio.h>
-int main() {
-    int m, n;
-    printf("Enter the number of rows and columns: \n");
-    scanf("%d %d", &m, &n);
-    int matrix[m][n], transpose[n][m];
-    printf("Enter the elements of the matrix:\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            transpose[j][i] = matrix[i][j];
-        }
-    }
-    printf("Original matrix:\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("Transpose of the matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("%d ", transpose[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main() {
+//     int m, n;
+//     printf("Enter the number of rows and columns: \n");
+//     scanf("%d %d", &m, &n);
+//     int matrix[m][n], transpose[n][m];
+//     printf("Enter the elements of the matrix:\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             scanf("%d", &matrix[i][j]);
+//         }
+//     }
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             transpose[j][i] = matrix[i][j];
+//         }
+//     }
+//     printf("Original matrix:\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             printf("%d ", matrix[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("Transpose of the matrix:\n");
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+//             printf("%d ", transpose[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
 
 
 // Assignment 6(3)
