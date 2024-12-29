089b004e1c2ff308dac12207c901829dd81ebb11
sudipto-mondal-05 <sudiptohwh2005@gmail.com>
2024-12-30 01:23:34 +0530

    A8 ok

--- A8.c
+++ A8.c
@@ -1,13 +1,13 @@
-//1.
+// 1.
 
 #include <stdio.h>
 
 float product(float a, int b) {
-    return a*b;
+    return a * b;
 }
 
 int main() {
-    float a,c;
+    float a, c;
     int b;
 
     printf("Enter a float number: ");
@@ -16,101 +16,92 @@ int main() {
     printf("Enter an integer number: ");
     scanf("%d", &b);
 
-    c = product(a,b);
+    c = product(a, b);
     printf("Product: %f\n", c);
 
     return 0;
 }
 
 
-
-//2.
+// 2.
 
 #include <stdio.h>
 
 void swap(int *a, int *b) {
-    int c = *a;
+    int temp = *a;
     *a = *b;
-    *b = c;
+    *b = temp;
 }
 
 int main() {
     int x, y;
-    
+
     printf("Enter two numbers: ");
     scanf("%d %d", &x, &y);
-    
+
     printf("Before swapping: x = %d, y = %d\n", x, y);
-    
+
     swap(&x, &y);
-    
+
     printf("After swapping: x = %d, y = %d\n", x, y);
-    
+
     return 0;
 }
 
 
-
-//3I.
+// 3I
 
 #include <stdio.h>
 
 int fact(int n) {
-    int f = 1, i;
-    for (i = 1; i <= n; i++) {
-        f = f*i;
+    int f = 1;
+    for (int i = 1; i <= n; i++) {
+        f *= i;
     }
     return f;
 }
 
 int main() {
-    int n,f;
+    int n, f;
+
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    f= fact(n);
+    f = fact(n);
 
-    printf("Factorial of %d: %d\n", n,f);
+    printf("Factorial of %d: %d\n", n, f);
 
     return 0;
 }
 
 
-//3II.
+// 3II
+
+#include <stdio.h>
 
 int fact(int a) {
-
-	if(a==1) {
-
-		return a;
-
-	} else {
-
-		return a*fact(a-1);
-
-	}
-
+    if (a == 1) {
+        return a;
+    } else {
+        return a * fact(a - 1);
+    }
 }
-
 
 int main() {
+    int n, f;
 
-int n,f;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-printf("Enter a number: ");
+    f = fact(n);
 
-scanf("%d", &n);
+    printf("Factorial of %d: %d\n", n, f);
 
-f= fact(n);
-
-printf("Factorial of %d: %d\n", n,f);
-
-return 0;
-
+    return 0;
 }
 
 
-//4I
+// 4I
 
 #include <stdio.h>
 
@@ -128,33 +119,33 @@ void fibonacci(int n) {
 
 int main() {
     int n;
+
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-    
+
     if (n >= 2) {
         fibonacci(n);
     } else {
         printf("Enter a number greater than or equal to 2\n");
     }
-    
+
     return 0;
 }
+
 
 // 4II
 
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    else
-        return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
     int n;
+
     printf("Enter the number of terms: ");
     scanf("%d", &n);
 
@@ -167,57 +158,46 @@ int main() {
 }
 
 
-// 5.
+// 5
 
 #include <stdio.h>
 
 int hcf(int a, int b) {
-    if (b == 0)
-        return a;
-    else
-        return hcf(b, a % b);
+    if (b == 0) return a;
+    else return hcf(b, a % b);
 }
 
 int main() {
-    int m,n,p;
-    
-    printf("Enter two numbers: ");
-    scanf("%d %d", &m,&n);
+    int m, n, p;
 
-    p=hcf(m,n);
-    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &m, &n);
+
+    p = hcf(m, n);
+
     printf("HCF: %d\n", p);
-    
+
     return 0;
 }
 
 
-
-
-
-//6.
+// 6
 
 #include <stdio.h>
 #include <math.h>
 
-int Quad(int a, int b, int c) {
+void Quad(int a, int b, int c) {
+    int d, x, y;
 
-int d,x,y;
+    d = (b * b) - (4 * a * c);
 
-d=(b*b)-(4*a*c);
-
-if (d>=0) {
-
-	x= ((-b)+ sqrt(d))/(2*a);
-	y= ((-b)- sqrt(d))/(2*a);
-
-	printf("The Real Roots Are x=%d and y=%d", x,y);
-
-} else if (d<0) {
-
-	printf("No Real Roots");
-}
-
+    if (d >= 0) {
+        x = ((-b) + sqrt(d)) / (2 * a);
+        y = ((-b) - sqrt(d)) / (2 * a);
+        printf("The Real Roots Are: x = %d and y = %d\n", x, y);
+    } else {
+        printf("No Real Roots\n");
+    }
 }
 
 int main() {
@@ -230,4 +210,3 @@ int main() {
 
     return 0;
 }
-
