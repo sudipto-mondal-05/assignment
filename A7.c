6892f864630802580aa95dcf0117138074433f74
sudipto_mondal_05 <sudiptohwh2005@gmail.com>
2025-01-19 04:00:15 +0530

    Add files via upload

--- A7.c
+++ A7.c
@@ -1,200 +1,76 @@
-// 1.
-
-#include <stdio.h>
-int main() {
-    char str[200];
-    int length = 0;
-    printf("Enter a string: ");
-    gets(str); 
-    while (str[length] != '\0') {
-        length++; 
-    }
-    printf("Length of the string: %d\n", length);
-    return 0;
-}
-
-// 2.
+// 1 
 
 // #include <stdio.h>
 
 // int main() {
-//     char str1[100], str2[100];
-//     int i = 0;
+//     char str[100];  
+//     int length = 0;
+//     char *ptr;
 
 //     printf("Enter a string: ");
-//     scanf("%[^\n]s", str1);
+//     fgets(str, sizeof(str), stdin); 
 
-//     while (str1[i] != '\0') {
+//     ptr = str; 
+
+//     while (*ptr != '\0' && *ptr != '\n') {
+//         length++;
+//         ptr++;
+//     }
+
+//     printf("The length of the string is: %d\n", length);
+
+//     return 0;
+// }
+
+// 2 
+
+// #include <stdio.h>
+// int main(){
+//     char str1[100];
+//     char str2[100];
+//     char *ptr;
+//     ptr = str1;
+
+//     printf("Enter you string: ");
+//     fgets(str1, sizeof(str1), stdin);
+    
+//     int i = 0;
+//     while(*ptr != '\0'){
 //         str2[i] = str1[i];
 //         i++;
-//     }
+//         ptr++;
+//     } 
 //     str2[i] = '\0';
-
-//     printf("Copied string: %s\n", str2);
+//     printf("The copied strinh is: %s", str2);
 //     return 0;
 // }
 
-// 3.
+// 3 
+ 
+ #include <stdio.h>
+int main(){
+    char str1[100];
+    char str2[100];
 
-// #include <stdio.h>
+    printf("Enter you 1st string: ");
+    fgets(str1, sizeof(str1), stdin);
+    printf("Enter you 2nd string: ");
+    fgets(str2, sizeof(str2), stdin);
 
-// int main() {
-//     char str1[100], str2[100];
-//     int i = 0, isEqual = 1;
+    int count = 0, i = 0;
 
-//     printf("Enter the first string: ");
-//     scanf("%s", str1);
-//     printf("Enter the second string: ");
-//     scanf("%s", str2);
+    while ( str1[i] != '\0' || str2[i] != '\0' ){
+        if(str1[i] != str2[i])
+            count++;
+        i++;
+    }
 
-//     while (str1[i] != '\0' || str2[i] != '\0') {
-//         if (str1[i] != str2[i]) {
-//             isEqual = 0;
-//             break;
-//         }
-//         i++;
-//     }
-
-//     if (isEqual) {
-//         printf("Strings are equal.\n");
-//     } else {
-//         printf("Strings are not equal.\n");
-//     }
-
-//     return 0;
-// }
-
-// 4.
-
-// #include <stdio.h>
-
-// int main() {
-//     char str1[100], str2[100];
-//     int i = 0, j = 0;
-
-//     printf("Enter the first string: ");
-//     scanf("%s", str1);
-//     printf("Enter the second string: ");
-//     scanf("%s", str2);
-
-//     while (str1[i] != '\0') {
-//         i++;
-//     }
-
-//     while (str2[j] != '\0') {
-//         str1[i] = str2[j];
-//         i++;
-//         j++;
-//     }
-//     str1[i] = '\0';
-
-//     printf("Concatenated string: %s\n", str1);
-//     return 0;
-// }
-
-// 5.
-
-// #include <stdio.h>
-
-// int main() {
-//     char str[100];
-//     int length = 0, isPalindrome = 1;
-
-//     printf("Enter a string: ");
-//     scanf("%s", str);
-
-//     while (str[length] != '\0') {
-//         length++;
-//     }
-
-//     for (int i = 0; i < length / 2; i++) {
-//         if (str[i] != str[length - i - 1]) {
-//             isPalindrome = 0;
-//             break;
-//         }
-//     }
-
-//     if (isPalindrome) {
-//         printf("The string is a palindrome.\n");
-//     } else {
-//         printf("The string is not a palindrome.\n");
-//     }
-
-//     return 0;
-// }
-
-// 6.
-
-// #include <stdio.h>
-
-// int main() {
-//     char str[100];
-//     int length = 0;
-
-//     printf("Enter a string: ");
-//     scanf("%s", str);
-
-//     while (str[length] != '\0') {
-//         length++;
-//     }
-
-//     printf("Reversed string: ");
-//     for (int i = length - 1; i >= 0; i--) {
-//         printf("%c", str[i]);
-//     }
-//     printf("\n");
-
-//     return 0;
-// }
-
-// 7.
-
-// #include <stdio.h>
-
-// int main() {
-//     char str[200];
-//     int words = 1, characters = 0;
-
-//     printf("Enter a line of text: ");
-//     scanf(" %[^\n]s", str);
-
-//     for (int i = 0; str[i] != '\0'; i++) {
-//         characters++;
-//         if (str[i] == ' ') {
-//             words++;
-//         }
-//     }
-
-//     printf("Number of words: %d\n", words);
-//     printf("Number of characters: %d\n", characters);
-//     return 0;
-// }
-
-// 8. 
-
-// #include <stdio.h>
-
-// int main() {
-//     char str[100];
-//     int vowels = 0, consonants = 0;
-
-//     printf("Enter a string: ");
-//     scanf("%s", str);
-
-//     for (int i = 0; str[i] != '\0'; i++) {
-//         char ch = str[i];
-//         if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-//             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-//                 ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-//                 vowels++;
-//             } else {
-//                 consonants++;
-//             }
-//         }
-//     }
-
-//     printf("Number of vowels: %d\n", vowels);
-//     printf("Number of consonants: %d\n", consonants);
-//     return 0;
-
-
+    if (count == 0){
+        printf("Strings are Equal.");
+    }
+    else{
+        printf("Strings are not Equal.");
+    }
+    
+    return 0;
+}
